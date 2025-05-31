ba1e02b172cc9e5ef418e7ddaa2c20b54379fdde
yassir777x <laaouissetyassir7@gmail.com>
2025-05-31 22:55:35 +0100

    Delete SCHOOL_MANAGEMENT_SYSTEM/STUDENT_MANAGEMENT_SYSTEM directory

--- SCHOOL_MANAGEMENT_SYSTEM/STUDENT_MANAGEMENT_SYSTEM/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=student_management_system" , 'root' , '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION) ;
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_ASSOC) ;
-} catch (PDOException $e) {
-    echo $e->getMessage() ;
-    die() ;
-}
