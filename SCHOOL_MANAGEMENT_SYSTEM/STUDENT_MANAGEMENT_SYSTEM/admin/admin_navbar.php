31a0f85bd21d2e5793a27350f4604f0ec293a45b
yassir777x <laaouissetyassir7@gmail.com>
2025-05-31 22:53:05 +0100

    Delete SCHOOL_MANAGEMENT_SYSTEM/STUDENT_MANAGEMENT_SYSTEM/admin/admin_navbar.php

--- SCHOOL_MANAGEMENT_SYSTEM/STUDENT_MANAGEMENT_SYSTEM/admin/admin_navbar.php
+++
@@ -1,8 +0,0 @@
-<nav class="nav-bar">
-    <div class="nav-bar-logo">
-        E-SCH<span>Oo</span>L
-    </div>
-    <div class="logout">
-        <a href="../logout.php">Log out</a>
-    </div>
-</nav>
